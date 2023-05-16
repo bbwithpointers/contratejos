437473304cf7b52e007a376454a412126db16605
Bruno B <brunog.barros@pm.me>
2023-05-16 19:45:41 -0300

    init project

---
+++ src/main/java/br/com/bruno4code/contratejos/ContratejosApplication.java
@@ -0,0 +1,13 @@
+package br.com.bruno4code.contratejos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratejosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratejosApplication.class, args);
+	}
+
+}
